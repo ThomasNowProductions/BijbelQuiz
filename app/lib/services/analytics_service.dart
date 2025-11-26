df0bb8e872d0a6882fd4952fa8f2fff5dd692da9
ThomasNow Productions <thomasnowprod@proton.me>
2025-11-26 10:52:09 +0100

    Updated tracking to posthog

--- app/lib/services/analytics_service.dart
+++ app/lib/services/analytics_service.dart
@@ -7,6 +7,7 @@ import '../providers/settings_provider.dart';
 import 'logger.dart';
 import 'tracking_service.dart';
 import 'package:uuid/uuid.dart';
+import 'package:posthog_flutter/posthog_flutter.dart';
 
 /// A service that provides an interface to the in-house tracking service.
 ///
@@ -29,8 +30,8 @@ class AnalyticsService {
     }
   }
 
-  /// Returns a [TrackingObserver] that can be used to automatically track screen views.
-  TrackingObserver getObserver() => _trackingService.getObserver();
+  /// Returns a [PosthogObserver] that can be used to automatically track screen views.
+  PosthogObserver getObserver() => _trackingService.getObserver();
 
   /// Tracks a screen view.
   ///
